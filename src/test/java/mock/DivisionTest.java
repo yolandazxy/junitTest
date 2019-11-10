5f9f52e42b7071bf2eae7988f2b64b7c228081da
yolandazxy <abagailzxy@qq.com>
2019-11-10 12:46:32 +0800

    [add]内部方法mock

--- src/test/java/mock/DivisionTest.java
+++ src/test/java/mock/DivisionTest.java
@@ -54,11 +54,26 @@ public class DivisionTest {
 
         //when(mockDivision.divid(0,0)).thenReturn(11);
         when(mockDivision.divid(anyInt(),anyInt())).thenReturn(11);  //改为任意值
+
         //when(mockDivision.divid(2,1)).thenReturn(1);
         System.out.println(mockDivision.divid(2,1));//若不指定，所有mock的方法都会把返回设为0
         //mockDivision.divid(2,1);
 
         assertThat(division2.divid2(0,0,5, mockDivision),equalTo(0));
+    }
 
+    @Test
+    public void divid3(){
+        assertThat(division2.divid3(100,10,5),equalTo(2));
+        Division2 mockDivision2 = mock(Division2.class);
+
+
+        when(mockDivision2.divid3(anyInt(),anyInt(),anyInt())).thenCallRealMethod(); //divid3调用原来方法
+
+        when(mockDivision2.divid(anyInt(),anyInt())).thenReturn(10);  //divid3内部调用的divid改为任意值
+        assertThat(mockDivision2.divid3(0,0,5),equalTo(2));
+
+        when(mockDivision2.divid(anyInt(),anyInt())).thenReturn(100);  //改为任意值
+        assertThat(mockDivision2.divid3(0,0,5),equalTo(0));
     }
 }
